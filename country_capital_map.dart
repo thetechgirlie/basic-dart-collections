def4bcd3e94f8165e1af9a773a4de27f95f9dccb
NnaemezieJuliet <ogochukwu.nnaemezie.247214@unn.edu.ng>
2024-11-18 21:01:20 +0100

    Add files via upload
    Add a map of countries with their capitals and print functionality

---
+++ country_capital_map.dart
@@ -0,0 +1,12 @@
+  // Create a map of countries and their capitals
+void main(List<String> args) {
+  final Countries = {'Nigeria': 'Abuja',
+  'USA': 'Washington',
+  'Ghana': 'Accra',
+
+  };
+
+//prints the map of countries and their capital
+print(Countries);
+
+  }
